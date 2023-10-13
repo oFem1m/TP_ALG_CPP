d44151a3bfee7558c275fcbf897083c23db09b9d
alex <volohovaleksandr1974@gmail.com>
2023-10-13 20:40:19 +0300

    tasks 2 edited

--- dz1/2.2.cpp
+++ dz1/2.2.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-/*Дан массив целых чисел А[0..n-1]. Известно, что на интервале [0, m] значения массива строго возрастают,
+/*Дан массив целых чисел A[0..n-1]. Известно, что на интервале [0, m] значения массива строго возрастают,
  а на интервале [m, n-1] строго убывают. Найти m за O(log m).*/
 
 int find_m(const int *A, int n) {
@@ -19,15 +19,20 @@ int find_m(const int *A, int n) {
     return low;
 }
 
-
 int main() {
-    int A[10000];
-    fill_n(A, 10000, 0);
-    int n = 0;
+    int n;
     cin >> n;
+
+    int* A = new int[n];
+
     for (int i = 0; i < n; ++i) {
         cin >> A[i];
     }
-    cout << find_m(A, n);
+
+    int m = find_m(A, n);
+    cout << m;
+
+    delete[] A;
+
     return 0;
 }
